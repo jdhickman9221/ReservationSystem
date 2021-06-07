2d53b705149c081f3ec11deb4521ecab1d952d04
Jdhickman9221 <jdhickman816@outlook.com>
2021-06-07 11:22:57 -0500

    +Created Configs Folder ++Added XML for Connections.config (db connection) ++Added XML for AppSecretKeys.config (email) +Ran Identity Samples +Replaced all instances of Identity Samples, replaced with project name

--- BenAxelHarveyStudio.UI.MVC/Startup.cs
+++ BenAxelHarveyStudio.UI.MVC/Startup.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(BenAxelHarveyStudio.UI.MVC.Startup))]
 namespace BenAxelHarveyStudio.UI.MVC
 {
     public partial class Startup
